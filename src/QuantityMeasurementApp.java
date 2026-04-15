c7230a17819d5022f25bb97c0befe3de29c9aa51
rakhshandaa <rakhshandayadav@gmail.com>
2026-04-15 10:07:38 +0530

    UC2: Feet and Inches measurement equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,43 +1,54 @@
 public class QuantityMeasurementApp {
 
-    // Inner class Feet
+    // Feet Class
     public static class Feet {
         private final double value;
 
-        // Constructor
         public Feet(double value) {
             this.value = value;
         }
 
-        // Override equals()
         @Override
         public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
 
-            // Same reference check (reflexive)
-            if (this == obj) {
-                return true;
-            }
-
-            // Null check + type check
-            if (obj == null || getClass() != obj.getClass()) {
-                return false;
-            }
-
-            // Cast safely
             Feet feet = (Feet) obj;
-
-            // Compare double values properly
             return Double.compare(this.value, feet.value) == 0;
         }
     }
 
+    // Inches Class (NEW in UC2)
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Inches inches = (Inches) obj;
+            return Double.compare(this.value, inches.value) == 0;
+        }
+    }
+
+    // Separate methods (IMPORTANT for UC2)
+    public static boolean compareFeet(double a, double b) {
+        return new Feet(a).equals(new Feet(b));
+    }
+
+    public static boolean compareInches(double a, double b) {
+        return new Inches(a).equals(new Inches(b));
+    }
+
     // Main method
     public static void main(String[] args) {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
 
-        boolean result = f1.equals(f2);
-
-        System.out.println("Are equal? " + result);
+        System.out.println("Feet equality: " + compareFeet(1.0, 1.0));
+        System.out.println("Inches equality: " + compareInches(1.0, 1.0));
     }
 }
